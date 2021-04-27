66a154db1d8cf4cd10c52e3fc0c12181df92006c
Felipe Borba <felipe.borba0109@gmail.com>
2021-04-27 20:41:18 -0300

    return statuscode

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -4,7 +4,6 @@ module.exports = app => {
     app.get('/attendance', (req, res) => res.send('attendance get route'))
 
     app.post('/attendance', (req, res) => {
-        Attendance.add(req.body)
-        res.send('attendance post route')
+        Attendance.add(req.body, res)
     })
 }
--- models/attendance.js
+++ models/attendance.js
@@ -2,7 +2,7 @@ const connection = require('../infrastructure/connection')
 const moment = require('moment')
 
 class Attendance {
-    add(attendance) {
+    add(attendance, response) {
         const createTime = moment().format('YYYY-MM-DD HH:MM:SS')
         const date = moment(attendance.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
         const enrichAttendance = {...attendance, createTime, date}
@@ -10,8 +10,10 @@ class Attendance {
         const sql = 'INSERT INTO Attendance SET ?'
         connection.query(sql, enrichAttendance, (error, result) => {
             if (error) {
+                response.status(400).json(error)
                 console.log(error)
             } else {
+                response.status(201).json(result)
                 console.log(result)
             }
         })
