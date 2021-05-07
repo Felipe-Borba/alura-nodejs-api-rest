1dd10e08eb3df58e734647a78e381e5adf3b22f6
Felipe Borba <felipe.borba0109@gmail.com>
2021-05-06 22:45:19 -0300

    02-04 return error message

--- file_system/fileUpload.js
+++ file_system/fileUpload.js
@@ -4,17 +4,19 @@ const path = require('path')
 module.exports = (filePath, fileName, callbackImageCreated) => {
     const validType = ['jpg', 'png', 'jpeg']
     const type = path.extname(filePath)
-    const validate = validType.indexOf(type.substring(1))
+    const validate = validType.indexOf(type.substring(1)) === -1
 
-    if (validate === -1) {
+    if (validate) {
+        const error = 'invalid image type'
         console.log('error: invalid image type');
+        callbackImageCreated(error) 
     } else {
         const newPath = `./assets/images/${fileName}${type}`
         // is there any difference between ./ and ../ ? 
         fs.createReadStream(filePath)
             .pipe(fs.createWriteStream(newPath))
             .on('finish', () => {
-                callbackImageCreated(newPath)
+                callbackImageCreated(false, newPath)
             })
     }
 }
--- models/pets.js
+++ models/pets.js
@@ -4,17 +4,22 @@ class pet {
     add(pet, response) {
         const query = `INSERT INTO pets SET ?`
 
-        upload(pet.image, pet.name, (newPath) => {
-            const newPet = { name: pet.name, image: newPath}
-            
-            connection.query(query, newPet, error => {
-                if (error) {
-                    console.log(error);
-                    response.status(400).json(error)
-                } else {
-                    response.status(200).json(newPet)
-                }
-            })
+        upload(pet.image, pet.name, (error, newPath) => {
+
+            if (error) {
+                response.status(400).json({error})
+            } else {
+                const newPet = { name: pet.name, image: newPath }
+
+                connection.query(query, newPet, error => {
+                    if (error) {
+                        console.log(error);
+                        response.status(400).json(error)
+                    } else {
+                        response.status(200).json(newPet)
+                    }
+                })
+            }
         })
 
     }
