840e084763df646e56483c3c8258927c41bc1445
Felipe Borba <felipe.borba0109@gmail.com>
2021-05-06 22:17:31 -0300

    02-03 add image extension check functionality

--- file_system/fileUpload.js
+++ file_system/fileUpload.js
@@ -1,12 +1,20 @@
 const fs = require('fs')
+const path = require('path')
 
-module.exports = (path, fileName, callbackImageCreated) => {
-    const newPath = `./assets/images/${fileName}`
-    // is there any difference between ./ and ../ ? 
-    fs.createReadStream(path)
-        .pipe(fs.createWriteStream(newPath))
-        .on('finish', () => {
-            callbackImageCreated(newPath)
-        })
+module.exports = (filePath, fileName, callbackImageCreated) => {
+    const validType = ['jpg', 'png', 'jpeg']
+    const type = path.extname(filePath)
+    const validate = validType.indexOf(type.substring(1))
 
+    if (validate === -1) {
+        console.log('error: invalid image type');
+    } else {
+        const newPath = `./assets/images/${fileName}${type}`
+        // is there any difference between ./ and ../ ? 
+        fs.createReadStream(filePath)
+            .pipe(fs.createWriteStream(newPath))
+            .on('finish', () => {
+                callbackImageCreated(newPath)
+            })
+    }
 }
